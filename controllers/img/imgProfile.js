f4005a0e6d24d341c1f4de6ee0d4c7ecc772f684
Adrián <adrianncatalan@gmail.com>
2022-05-22 15:09:03 +0200

    últimos cambios

--- controllers/img/imgProfile.js
+++ controllers/img/imgProfile.js
@@ -1,10 +1,7 @@
 const { request, response } = require('express');
+const { uploadFile } = require('../../helpers/uploadFile');
 
-const path = require('path');
-
-const { v4: uuidv4 } = require('uuid');
-
-const uploadImgProfile = (req = request, res = response) => {
+const uploadImgProfile = async (req = request, res = response) => {
 
     if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
         response.status(400).json({ msg: 'No hay archivos para subir' });
@@ -12,33 +9,10 @@ const uploadImgProfile = (req = request, res = response) => {
         return;
     }
 
+    const nameFile = await uploadFile(req.files);
 
-
-    const { archivo } = req.files;
-
-    const nombreCortado = archivo.name.split('.');
-
-    const extension = nombreCortado[nombreCortado.length - 1];
-
-    //Validar extensión
-    const extensionesValidas = ['png', 'jpg', 'jpeg'];
-
-    if (!extensionesValidas.includes(extension)) {
-        return res.status(400).json({
-            msg: 'La extensión .' + extension + ' no es permitida'
-        })
-    }
-
-    const nombreTemp = uuidv4() + '.' + extension;
-
-    const uploadPath = path.join(__dirname, '../../uploads/' + nombreTemp);
-
-    archivo.mv(uploadPath, (err) => {
-        if (err) {
-            return response.status(500).send(err);
-        }
-        res.send('Archivo subido a la ruta' + uploadPath);
-        console.log(path.join(__dirname, '../../uploads/' + archivo.name));
+    res.json({
+        nameFile
     });
 }
 
---
+++ helpers/uploadFile.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+const { v4: uuidv4 } = require('uuid');
+
+const uploadFile = (files, extensionesValidas = ['png', 'jpg', 'jpeg'], imgProfile = '') => {
+
+    return new Promise((resolve, reject) => {
+
+        const { archivo } = files;
+
+        const nombreCortado = archivo.name.split('.');
+
+        const extension = nombreCortado[nombreCortado.length - 1];
+
+        //Validar extensión
+        if (!extensionesValidas.includes(extension)) {
+            return reject('La extensión .' + extension + ' no es permitida')
+        }
+
+        const nombreTemp = uuidv4() + '.' + extension;
+
+        const uploadPath = path.join(__dirname, '../uploads/', imgProfile + nombreTemp);
+
+        archivo.mv(uploadPath, (err) => {
+            if (err) {
+                return response.status(500).send(err);
+            }
+            resolve(nombreTemp);
+            console.log(path.join(__dirname, '../uploads/' + archivo.name));
+        });
+    })
+}
+
+module.exports.uploadFile = uploadFile;
