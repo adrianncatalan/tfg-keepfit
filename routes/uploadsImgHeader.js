8a6a1d434ae683a235ef672b094466b2ba3f017c
Adrián <adrianncatalan@gmail.com>
2022-05-23 14:35:09 +0200

    Last changes

--- routes/uploadsImgHeader.js
+++ routes/uploadsImgHeader.js
@@ -1,28 +1,28 @@
-//Requerimos esta función del paquete express para movernos por rutas
-const { Router } = require('express');
+// //Requerimos esta función del paquete express para movernos por rutas
+// const { Router } = require('express');
 
-//Importamos check de express-validator
-//Nos va permitir ejecutar todos los middlewares para verificar los campos, antes de disparar la ruta
-const { check } = require('express-validator')
+// //Importamos check de express-validator
+// //Nos va permitir ejecutar todos los middlewares para verificar los campos, antes de disparar la ruta
+// const { check } = require('express-validator')
 
-//Importamos nuestro archivo de validaciones
-const { validation, validarImg } = require('../middleware')
+// //Importamos nuestro archivo de validaciones
+// const { validation, validarImg } = require('../middleware')
 
-const { uploadsImgHeader, updateImgHeader } = require('../controllers');
+// const { uploadsImgHeader, updateImgHeader } = require('../controllers');
 
-const { coleccionesPermitidas } = require('../helpers/dbValidators');
+// const { coleccionesPermitidas } = require('../helpers/dbValidators');
 
-//Creamos una constante que guarde las propiedades Router
-const router = Router();
+// //Creamos una constante que guarde las propiedades Router
+// const router = Router();
 
-router.post('/', validarImg, uploadsImgHeader);
+// router.post('/', validarImg, uploadsImgHeader);
 
-router.put('/:coleccion/:id', [
-    validarImg,
-    check('id', 'El id debe ser de mongo').isMongoId(),
-    check('coleccion').custom(c => coleccionesPermitidas(c, ['users', 'sessions', 'roles'])),
-    validation
-], updateImgHeader);
+// router.put('/:coleccion/:id', [
+//     validarImg,
+//     check('id', 'El id debe ser de mongo').isMongoId(),
+//     check('coleccion').custom(c => coleccionesPermitidas(c, ['users', 'sessions', 'roles'])),
+//     validation
+// ], updateImgHeader);
 
-//Exportamos el router
-module.exports = router;
+// //Exportamos el router
+// module.exports = router;
