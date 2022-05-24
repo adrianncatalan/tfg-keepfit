b44e03757addd08fa191b4c48cb2c091b6b2d4df
Adrián <adrianncatalan@gmail.com>
2022-05-24 09:44:18 +0200

    Last changes

--- public/js/cookies.js
+++ public/js/cookies.js
@@ -12,17 +12,21 @@ window.addEventListener("load", () => {
     p.textContent = "We have made important changes to our privacy and cookie policy and want you to know what this means for you and your data."
 
     const buttonAccept = document.createElement("button");
-    buttonAccept.textContent = "Yes"
+    buttonAccept.textContent = "Accept"
     buttonAccept.className="cookies_button"
     buttonAccept.id = "buttonAccept"
 
     const section_button = document.createElement("div");
     section_button.className="cookies_buttons"
 
-    
+    const linkCookies= document.createElement('a');
+
+    linkCookies.href = '/cookiePolicy';
+
+    linkCookies.textContent = 'Read';
 
     const buttonReadCookiesPolicy = document.createElement("button");
-    buttonReadCookiesPolicy.textContent = "Read more"
+    // buttonReadCookiesPolicy.textContent = "Read"
     buttonReadCookiesPolicy.className="cookies_button"
 
     const footer = document.getElementById("footer")
@@ -30,6 +34,7 @@ window.addEventListener("load", () => {
     section.appendChild(h2);
     section.appendChild(p);
     section.appendChild(section_button);
+    buttonReadCookiesPolicy.append(linkCookies);
     section_button.appendChild(buttonReadCookiesPolicy);
     section_button.appendChild(buttonAccept);
 
